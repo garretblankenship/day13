bbe554220891c73703fe73e762d0a0282942a285
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-06-13 11:26:38 +1000

    adding in routes

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ app.use(express.json());
 
 const contacts = [];
 
+app.get("/contacts", (req, res) => {
+
+});
+
+app.post("/contacts", (req, res) => {
+    const { name, email } = req.body;
+    const contact = { name, email };
+    contacts.push(contact);
+
+    res.redirect("/contacts");
+});
+
+app.get("/contacts/new", (req, res) => {
+    res.render("contacts/form");
+});
+
 app.listen(3000, () => {
     console.log(`The server is running on port 3000`);
 })
